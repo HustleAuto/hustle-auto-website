3cd2a04c9d202e32737a7dea459f1e6709210dc0
Pranav Bodawala <b.pranav.k@gmail.com>
2024-07-10 06:08:56 +0000

    feat: :sparkles: Create disclaimer page

---
+++ src/app/legal/disclaimer/page.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+export default function Page() {
+  return (
+    <>
+      <main className="container mx-auto my-20 prose">
+        <h1 id="disclaimer">Disclaimer</h1>
+
+        <p>Customer agrees with all the information below</p>
+        <p>
+          While it our intent to provide you with the BEST available service and
+          the finest workmanship in reconditioning of your vehicle, it is
+          necessary for us to DISCLAIM TOTAL LIABILITY FOR ALL DAMAGES including
+          but not limited to the following: Engine Damages, Further Paint
+          Damages, Tear to the Interior of worn material, Cracks in the
+          windshield due to pre-existing chips.
+        </p>
+        <p>
+          For Safety and Liability reason Hustle Automotive Services and its
+          employees cannot remove the following: Car Seats, any and all forms of
+          child restraints, any and all forms of removable seats from any type
+          of vehicle.
+        </p>
+        <p>
+          Pictures showing customer vehicles maybe be used in advertisements
+          published by Hustle Automotive Services. No personal information
+          related to the customer will be released without prior permission from
+          customer.
+        </p>
+        <p>
+          Customers are required to look over the vehicle during pickup. No
+          refunds shall be issued once the customer has taken the vehicle home.
+        </p>
+      </main>
+    </>
+  );
+}
